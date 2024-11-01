3220d2655677696168bf2f0bcabc0d98f98308c4
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-11-01 22:33:24 +0500

    add

--- src/context/api/index.js
+++ src/context/api/index.js
@@ -4,7 +4,7 @@ import { logout } from "../slices/authSlice";
 const baseQuery = async (args, api, extraOptions) => {
   const { dispatch } = api;
   const rawBaseQuery = fetchBaseQuery({
-    baseUrl: "https://shopping.azizbek2002.uz",
+    baseUrl: "https://ergashaliyev.uz",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("x-auth-token");
       if (token) {
